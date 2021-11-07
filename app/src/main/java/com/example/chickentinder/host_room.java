181ffe7a598c50553327fbea6a71d6e256364129
Karlo Tino <karloangelo.tino.iics@ust.edu.ph>
2021-11-07 15:14:08 +0800

    fixed drawable prob. added button id and use main to join added host room file 0 input

---
+++ app/src/main/java/com/example/chickentinder/host_room.java
@@ -0,0 +1,14 @@
+package com.example.chickentinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class host_room extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host_room);
+    }
+}
