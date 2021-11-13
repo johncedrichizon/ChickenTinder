53f09aad34f23746832dbaa0cd7664d874d84f4b
johncedrichizon <johncedric.hizon.iics@ust.edu.ph>
2021-11-13 13:07:17 +0800

    host room ui update

--- app/src/main/java/com/example/chickentinder/MainActivity.java
+++ app/src/main/java/com/example/chickentinder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.chickentinder;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,22 +9,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button jr_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button jr_main = (Button) findViewById(R.id.jr_main);
-
-        jr_main.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            Intent a = new Intent(getApplicationContext(),join_page.class);
-            startActivity(a);
+        Button btnMainHost = (Button) findViewById(R.id.btnMainHost);
+        Button btnMainJoin = (Button) findViewById(R.id.btnMainJoin);
 
 
-            }
-        });
+
     }
 }
