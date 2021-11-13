113845808a24356cd29263fa68539ab3e4d8b7a0
johncedrichizon <johncedric.hizon.iics@ust.edu.ph>
2021-11-13 13:43:07 +0800

    host room ui update

--- app/src/main/java/com/example/chickentinder/MainActivity.java
+++ app/src/main/java/com/example/chickentinder/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.chickentinder;
 
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +17,21 @@ public class MainActivity extends AppCompatActivity {
         Button btnMainHost = (Button) findViewById(R.id.btnMainHost);
         Button btnMainJoin = (Button) findViewById(R.id.btnMainJoin);
 
+        btnMainHost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(MainActivity.this, HostRoom.class);
+                startActivity(a);
+            }
+        });
 
+        btnMainJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent b = new Intent(MainActivity.this, join_page.class);
+                startActivity(b);
+            }
+        });
 
     }
 }
